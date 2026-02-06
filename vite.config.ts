c5e73b6107acf5b1dad3b2782577ba812f6d0b8b
Dmytro Koniushenko <eazy777er@gmail.com>
2026-02-06 17:28:17 +0100

    Project preparation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+        // Använd den port som står i .env
+        '/api': 'http://localhost:1337/'
+    }
+  }
 })
